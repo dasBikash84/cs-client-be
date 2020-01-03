c36c19f7285916756ee07c183398e35f21a941e4
Bikash Das <bikashdaseee@gmail.com>
2020-01-03 15:18:32 +0600

    Init commit.

---
+++ src/main/kotlin/com/dasbikash/csclientbe/CsClientBeApplication.kt
@@ -0,0 +1,11 @@
+package com.dasbikash.csclientbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsClientBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsClientBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/dasbikash/csclientbe/CsClientBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dasbikash.csclientbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsClientBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
