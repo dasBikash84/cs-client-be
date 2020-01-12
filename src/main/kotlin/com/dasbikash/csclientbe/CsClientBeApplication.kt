43ffe7052c2a112f5a06bcfe5b3f38298fcb3c80
Bikash Das <das.bikash.asl@gmail.com>
2020-01-12 10:37:48 +0600

    'SpringBootServletInitializer' added.

--- src/main/kotlin/com/dasbikash/csclientbe/CsClientBeApplication.kt
+++ src/main/kotlin/com/dasbikash/csclientbe/CsClientBeApplication.kt
@@ -1,10 +1,17 @@
 package com.dasbikash.csclientbe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class CsClientBeApplication
+class CsClientBeApplication: SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder? {
+		return application.sources(CsClientBeApplication::class.java)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<CsClientBeApplication>(*args)
